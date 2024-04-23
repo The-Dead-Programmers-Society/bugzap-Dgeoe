722fbd45ad8623aa8fcc6fe00441d6f8fa212116
Dgeoe <joedoshea15@gmail.com>
2024-04-23 14:18:23 +0100

    Created an array of sound effect files that are assigned to a Random Index to add more variety to the drag and drop keys

--- 2024Music/TextureRect.gd
+++ 2024Music/TextureRect.gd
@@ -1,5 +1,22 @@
 extends TextureRect
 
+func playRandomSound():
+	var audioPlayer1 = $AudioStreamPlayer2D
+	var audioPlayer2 = $AudioStreamPlayer2D2
+	var audioPlayer3 = $AudioStreamPlayer2D3
+	var audioPlayer4 = $AudioStreamPlayer2D4
+	
+	var randomIndex = randi() % 4 + 1  # Generates a random number between 1 to 4
+	match randomIndex:
+		1:
+			audioPlayer1.play()
+		2:
+			audioPlayer2.play()
+		3:
+			audioPlayer3.play()
+		4:
+			audioPlayer4.play()
+
 #triggers when you click and drag
 func _get_drag_data(at_position):
 	
@@ -16,6 +33,8 @@ func _get_drag_data(at_position):
 	texture = null
 	
 	return preview_texture.texture
+	#play random sound
+	playRandomSound()
 
 #triggers when you hover with dragged item
 func _can_drop_data(_pos, data):
@@ -24,4 +43,5 @@ func _can_drop_data(_pos, data):
 #triggers when you drop that dragged item
 func _drop_data(_pos, data):
 	texture = data
+	playRandomSound()
 	
