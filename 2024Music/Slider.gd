60c7362119710f509d7ef7c0bef0adbf47839bb0
Dgeoe <joedoshea15@gmail.com>
2024-04-23 15:41:54 +0100

    Volume Slider

---
+++ 2024Music/Slider.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+var master_bus = AudioServer.get_bus_index("Master")
+
+
+func _on_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value)
+	
+	if value == -30:
+		AudioServer.set_bus_mute(master_bus,true)
+	else:
+		AudioServer.set_bus_mute(master_bus, false)
+	pass # Replace with function body.
