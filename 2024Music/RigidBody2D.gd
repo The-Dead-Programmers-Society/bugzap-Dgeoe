707276894b98594761d48afb75453c2471ef7e87
Dgeoe <joedoshea15@gmail.com>
2024-04-24 01:06:13 +0100

    Added crank that can be spun with arrow keys, the speed it is spun will dicatate its pitch

---
+++ 2024Music/RigidBody2D.gd
@@ -0,0 +1,34 @@
+extends RigidBody2D
+
+const FORCE_AMOUNT = 202200  # Force applied
+
+var soundPlayer: AudioStreamPlayer2D
+
+func _ready():
+	soundPlayer = $AudioStreamPlayer2D  
+
+func _process(delta):
+	if Input.is_action_pressed("move_right"):
+		apply_force(Vector2.LEFT * FORCE_AMOUNT, Vector2.ZERO)
+		play_sound_effect()
+	elif Input.is_action_pressed("move_left"):
+		apply_force(Vector2.RIGHT * FORCE_AMOUNT, Vector2.ZERO)
+		play_sound_effect()
+
+func play_sound_effect():
+	if soundPlayer:
+		# Generate a random pitch value
+		var randomPitch = generate_random_pitch()
+		
+		# Set the pitch of the audio player
+		soundPlayer.pitch_scale = randomPitch
+		
+		# Play the audio with the randomized pitch
+		soundPlayer.play()
+
+# Generate a random pitch value
+func generate_random_pitch():
+	var minPitch = 0.8
+	var maxPitch = 1.2
+	return randf_range(minPitch, maxPitch)
+
